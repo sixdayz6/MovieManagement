7df9987d67e36514b8d563761b030de3bd79c8f9
sixdayz6 <70253711+sixdayz6@users.noreply.github.com>
2025-09-10 11:45:23 +0900

    main.py

---
+++ Main.py
@@ -0,0 +1,36 @@
+MovieList = []
+CommentList = []
+RateList = []
+
+while True:
+    choice = input('''
+                다음 기능 중에서 선택해주세요.
+                C.조회
+                I.입력
+                D.삭제
+                U.수정
+                R.영화평점 매기기
+                M.영화 댓글 남기기
+                V.댓글 조회
+                Q.종료
+                   ''').upper()
+    
+    if choice == "C":
+        pass
+    elif choice == "I":
+        pass
+    elif choice == "D":
+        pass
+    elif choice == "U":
+        pass
+    elif choice == "R":
+        pass
+    elif choice == "M":
+        pass
+    elif choice == "V":
+        pass
+    elif choice == "Q":
+        break
+    else:
+        print("기능들 중에서 선택해 주세요.")
+    
